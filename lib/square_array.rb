004d3c7852d0a8aae721ff76cc9b4c863c20a5e3
Mike Pottebaum <mpottebaum@gmail.com>
2020-01-25 02:54:28 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,19 @@
+#def square_array(array)
+#  i = 0
+#  new_array = []
+#  while i < array.length do
+#    square = array[i] * array[i]
+#    new_array.push(square)
+#    i += 1
+#  end
+#  new_array
+#end
+
 def square_array(array)
-  i = 0
   new_array = []
-  while i < array.length do
+  for n in array
     square = array[i] * array[i]
-    new_array.push(square)
-    i += 1
+    new_array.push()
   end
   new_array
 end
